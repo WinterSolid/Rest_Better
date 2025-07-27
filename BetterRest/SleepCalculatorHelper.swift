1396e5f63e36c7825845f2517e1b3c6f6f723eb3
ZakTan <zakee.tanks@gmail.com>
2025-07-27 15:17:44 -0700

    Helper for testing

--- BetterRest/SleepCalculatorHelper.swift
+++ BetterRest/SleepCalculatorHelper.swift
@@ -1,5 +1,5 @@
 //
-//  SleepCalculatorHelper.swift.swift
+//  SleepCalculatorHelper.swift
 //  BetterRest
 //
 //  Created by Wintersolid Studios on 7/27/25.
@@ -11,7 +11,7 @@ import CoreML
 struct SleepCalculatorHelper {
     static func calculateBedtime(wakeUp: Date, sleepAmount: Double, coffeeAmount: Int) throws -> Date {
         let config = MLModelConfiguration()
-        let model = try SleepCalculator(configuration: config)
+        let model = try SleepCalculatorModel(configuration: config)
 
         let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
         let hour = (components.hour ?? 0) * 60 * 60
