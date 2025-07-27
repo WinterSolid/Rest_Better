de4b35fb4d20815d78c6ae831d94a372e05f508d
ZakTan <zakee.tanks@gmail.com>
2025-07-26 18:48:32 -0700

    Ui adustmant vor picker values, indent file

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -14,48 +14,63 @@ struct ContentView: View {
   
   var coloredSleepAmountValues: some View {
     Text("\(sleepAmount.formatted())")
+      .font(.title2)
+      .fontWeight(.bold)
       .foregroundColor(sleepAmount < 7 ? .blue : .red)
   }
   var coloredcoffeeAmountValues: some View{
-    Text("\(coffeeAmount)").foregroundStyle(coffeeAmount < 3 ? .blue : .orange )
+    Text("\(coffeeAmount)")
+      .font(.title2)
+      .fontWeight(.bold)
+      .foregroundStyle(coffeeAmount < 4 ? .blue : .orange)
   }
   
   var body: some View {
     NavigationStack {
-      VStack(spacing: 30){
-        Spacer()
-        Text("When do you want to wake up")
-          .font(.headline)
-        Text("Pick time to sleep:").font(.title)
-        DatePicker("Pick time to sleep:", selection: $wakeUp,displayedComponents:.hourAndMinute).labelsHidden()
-        
-        
-        Stepper(value: $sleepAmount, in: 1...12, step: 1) {
-          HStack {
-            Text("How many hours of sleep:").font(.title2)
-            coloredSleepAmountValues
-          }.padding()
+      ZStack{
+        Color.gray.opacity(0.2)
+        VStack(spacing: 30){
+          
+          Spacer()
+          
+          Text("When do you want to wake up")
+            .font(.headline)
+          Text("Pick time to sleep:").font(.title)
+          
+          DatePicker("Pick time to sleep:", selection: $wakeUp,displayedComponents:.hourAndMinute).labelsHidden()
+          
+          Stepper(value: $sleepAmount, in: 1...12, step: 1) {
+            
+            HStack {
+              Text("Sleep # of hours:")
+                .font(.title3)
+              coloredSleepAmountValues
+            }.padding()
+          }
+          
+          Stepper(value: $coffeeAmount, in: 1...12, step: 1) {
+            HStack {
+              Text("Coffee cups drank today:")
+                .font(.title3)
+              coloredcoffeeAmountValues
+            }.padding()
+          }
+          
+          Spacer()
+          
+          Button(action: calculateSleepTime) {
+            Text("Calculate")
+          }
+          .font(.title)
+          .foregroundStyle(.green)
+          .buttonStyle(.bordered)
+          .buttonBorderShape(.capsule)
+          .tint(.green)
+          
+          Spacer()
         }
-        
-        Stepper(value: $coffeeAmount, in: 1...12, step: 1) {
-          HStack {
-            Text("How many cups of coffee:").font(.title2)
-            coloredcoffeeAmountValues
-          }.padding()
-        }
-        Spacer()
-        Button(action: calculateSleepTime) {
-          Text("Calculate")
-        }
-        .font(.title)
-        .foregroundStyle(.green)
-        .buttonStyle(.bordered)
-        .buttonBorderShape(.capsule)
-        .tint(.green)
-        
-        Spacer()
+        .navigationTitle("BetterRest").navigationBarTitleDisplayMode(.inline)
       }
-      .navigationTitle("BetterRest").navigationBarTitleDisplayMode(.inline)
     }
   }
   func calculateSleepTime() {
